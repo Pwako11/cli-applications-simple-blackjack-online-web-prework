80705e2f0393304c59cf24a08b5a662924d95600
Patrick A <akolo@hotmail.com>
2020-04-07 09:59:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -66,8 +66,9 @@ def runner
   until card_total > 21
     card_total = hit?(card_total)
     display_card_total(card_total)
-    end_game(deal_card)
-  end 
+    
+  end
+  end_game(deal_card)
 end
     
 #binding.pry
